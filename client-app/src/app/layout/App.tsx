5a78fdc8b2a17c2955af2d413082863c45fd1958
이희영 <gl171111@gmail.com>
2020-04-14 10:38:19 +0900

    [client-app] App컴포넌트를 React hooks를 이용하여 클래스 컴포넌트에서 함수형 컴포넌트로 변경

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,42 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Header, Icon } from 'semantic-ui-react';
 import { List } from 'semantic-ui-react';
+
 import { IActivity } from '../models/activity';
 
-interface IState {
-  activities: IActivity[];
-}
-
-class App extends React.Component<{}, IState> {
-  readonly state: IState = {
-    activities: [],
-  };
-  componentDidMount() {
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+  useEffect(() => {
     axios
       .get<IActivity[]>('http://localhost:5000/api/activities')
       .then((response) => {
-        console.log(response);
-        this.setState({
-          activities: response.data,
-        });
+        setActivities(response.data);
       });
-  }
-  render() {
-    return (
-      <div className='App'>
-        <Header as='h2'>
-          <Icon name='users' />
-          <Header.Content>Reactivity</Header.Content>
-        </Header>
-        <List>
-          {this.state.activities.map((activity) => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-        </List>
-      </div>
-    );
-  }
-}
+    //if I remove the array here then what's going to happens is I'am going to send my component into loop,
+    //because if I don't have this here then there's noting to stop this from running every single time my component renders.
+  }, []);
+
+  return (
+    <div className='App'>
+      <Header as='h2'>
+        <Icon name='users' />
+        <Header.Content>Reactivity</Header.Content>
+      </Header>
+      <List>
+        {activities.map((activity) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+};
 
 export default App;
