343c8afc402a3aaf941262bf17d3a83250661599
이희영 <gl171111@gmail.com>
2020-04-15 16:56:48 +0900

    React 클라이언트 사이 CRUD 구현

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,34 +1,80 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Header, Icon } from 'semantic-ui-react';
-import { List } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 
 import { IActivity } from '../models/activity';
+import NavBar from '../../features/nav/NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+    null
+  ); //union type : Selected activity can be a type of activity or it can be null
+  const [editMode, setEditMode] = useState(false);
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.filter((a) => a.id === id)[0]);
+    setEditMode(false);
+  };
+
+  const handleOpenCreateForm = () => {
+    setSelectedActivity(null);
+    setEditMode(true);
+  };
+
+  const handleCreateActivity = (activity: IActivity) => {
+    setActivities([...activities, activity]);
+    setSelectedActivity(activity);
+    setEditMode(false);
+  };
+
+  const handleEditActivity = (activity: IActivity) => {
+    setActivities([
+      ...activities.filter((a) => a.id !== activity.id),
+      activity,
+    ]);
+    setSelectedActivity(activity);
+    setEditMode(false);
+  };
+
+  const handleDeleteActivity = (id: string) => {
+    setActivities([...activities.filter((a) => a.id !== id)]);
+  };
   useEffect(() => {
     axios
       .get<IActivity[]>('http://localhost:5000/api/activities')
       .then((response) => {
-        setActivities(response.data);
+        let activities: IActivity[] = [];
+        response.data.forEach((activity) => {
+          activity.date = activity.date.split('.')[0];
+          activities.push(activity);
+        });
+        setActivities(activities);
       });
     //if I remove the array here then what's going to happens is I'am going to send my component into loop,
     //because if I don't have this here then there's noting to stop this from running every single time my component renders.
   }, []);
 
   return (
-    <div className='App'>
-      <Header as='h2'>
-        <Icon name='users' />
-        <Header.Content>Reactivity</Header.Content>
-      </Header>
-      <List>
-        {activities.map((activity) => (
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        ))}
-      </List>
-    </div>
+    <>
+      <NavBar openCreateForm={handleOpenCreateForm} />
+      <Container style={{ marginTop: '7em' }}>
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectActivity}
+          selectedActivity={selectedActivity!} //This is basically going to define it as either an activity or null 또 한가지 방법은 인터페이스 프롭 정의할 때 IActivity | null 이렇게 설정해주는 것
+          editMode={editMode}
+          setEditMode={setEditMode}
+          setSelectedActivity={setSelectedActivity}
+          createActivity={handleCreateActivity}
+          editActivity={handleEditActivity}
+          deleteActivity={handleDeleteActivity}
+        />
+        \
+      </Container>
+    </>
   );
 };
 
